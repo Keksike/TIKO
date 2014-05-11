170d67a6b2b0fa47b36741037251eba24fc5305a
Rougle <ossi.puustinen@uta.fi>
2014-05-11 22:11:56 +0300

    Pientä päivitystyä
    Virheviesti vakioitu. HaeTLkm() hieman päivitetty

--- TietokantaToiminnot.java
+++ TietokantaToiminnot.java
@@ -17,403 +17,401 @@ import java.util.Scanner;
 
 public class TietokantaToiminnot {
 
-	  // tietokannan ja käyttäjän tiedot
+    // tietokannan ja käyttäjän tiedot
 
-	private final String AJURI = "org.postgresql.Driver";
-	private final String PROTOKOLLA = "jdbc:postgresql:";
-	private final String PALVELIN = "dbstud.sis.uta.fi";
-	private final int PORTTI = 5432;
-	private final String TIETOKANTA = "tiko2014db29";  // tähän oma käyttäjätunnus
-	private final String KAYTTAJA = "";  // tähän oma käyttäjätunnus
-	private final String SALASANA = "";  // tähän tietokannan salasana
+    private final String AJURI = "org.postgresql.Driver";
+    private final String PROTOKOLLA = "jdbc:postgresql:";
+    private final String PALVELIN = "dbstud.sis.uta.fi";
+    private final int PORTTI = 5432;
+    private final String TIETOKANTA = "tiko2014db29";  // tähän oma käyttäjätunnus
+    private final String KAYTTAJA = "";  // tähän oma käyttäjätunnus
+    private final String SALASANA = "";  // tähän tietokannan salasana
+   
+    private final String VIRHE = "Tapahtui virhe.";
 
-	private Connection con;
-	private Statement stmt;
+    private Connection con;
+    private Statement stmt;
 
-	//Yhteyden avaaminen
-	public boolean avaaYhteys(){
-
-
-		// Vaihe 1: tietokanta-ajurin lataaminen
-		try {
-			Class.forName(AJURI);
-		} catch (ClassNotFoundException poikkeus) {
-			System.out.println("Ajurin lataaminen ei onnistunut. Lopetetaan ohjelman suoritus.");
-			return false;
-		}
-
-		// Vaihe 2: yhteyden ottaminen tietokantaan
-		con = null;
-		try {
-			con = DriverManager.getConnection(PROTOKOLLA + "//" + PALVELIN + ":" + PORTTI + "/" + TIETOKANTA, KAYTTAJA, SALASANA);
-		  
-			stmt = con.createStatement();
-
-		// Toiminta mahdollisessa virhetilanteessa
-		} catch (SQLException poikkeus) {
-			System.out.println("Tapahtui seuraava virhe: " + poikkeus.getMessage());
-			return false;
-		}
-
-		return true;
-
-	}
+    //Yhteyden avaaminen
+    public boolean avaaYhteys(){
 
 
-	//Yhteyden sulkeminen
-	public boolean suljeYhteys(){
+    // Vaihe 1: tietokanta-ajurin lataaminen
+    try {
+        Class.forName(AJURI);
+    } catch (ClassNotFoundException poikkeus) {
+        System.out.println("Ajurin lataaminen ei onnistunut. Lopetetaan ohjelman suoritus.");
+        return false;
+    }
 
-		// yhteyden sulkeminen 
-	 
-		if (con != null) { // jos yhteyden luominen ei onnistunut, con == null
-
-			try {     
-				con.close();
-				stmt.close(); // Sulkee myös tulosjoukon    
-				return true;
-
-			} catch(SQLException poikkeus) {
-				System.out.println("Yhteyden sulkeminen tietokantaan ei onnistunut. Lopetetaan ohjelman suoritus.");
-
-				return false; //null
-			}
-
-		}else{
-			System.out.println("Yhteyttä tietokantaan ei ollut. Yhteyden sulkeminen tietokantaan ei onnistunut.");
-			return false;
-		}
-
-	}
-
-
-	//Haetaan tehtavalista
-	public ResultSet haeTehtLista(int listaNro){
-
-		ResultSet rs = null; // Kyselyn tulokset
-
-		try {
-			
-			String lause = "SELECT id, kuvaus FROM tehtavalista WHERE id = " + listaNro + ";";
-
-			rs = lahetaKysely(lause);
-
-		} catch (Exception poikkeus) {
-			System.out.println("Jokin meni pieleen.");
-			return null;
-		}
-
-		return rs; //Palautetaan tulosjoukko
-
-	}
-
-	//Haetaan tehtävä
-	public ResultSet haeTehtava(int tehtNro, int tehtLista){
-
-		ResultSet rs = null; // Kyselyn tulokset
-		try {            
-		String lause = "SELECT tehtava.id, tehtava.kuvaus " + 
-			"FROM tehtava, kuuluu, tehtavalista " + 
-			"WHERE tehtava.id = " + tehtNro + 
-			" AND tehtavalista.id = " + tehtLista + 
-			" AND tehtava.id = kuuluu.tehtava_id AND kuuluu.tehtavalista_id = tehtavalista.id;";
-
-		rs = lahetaKysely(lause);
-
-		} catch (Exception poikkeus) {
-			System.out.println("Jokin meni pieleen.");
-			return null;
-		}
+    // Vaihe 2: yhteyden ottaminen tietokantaan
+    con = null;
+    try {
+        con = DriverManager.getConnection(PROTOKOLLA + "//" + PALVELIN + ":" + PORTTI + "/" + TIETOKANTA, KAYTTAJA, SALASANA);
         
-		return rs; //Palautetaan tulosjoukko
+        stmt = con.createStatement();
 
-	}
+    // Toiminta mahdollisessa virhetilanteessa
+    } catch (SQLException poikkeus) {
+        System.out.println("Tapahtui seuraava virhe: " + poikkeus.getMessage());
+        return false;
+    }
 
-	//Hae esimerkkitietokanta
-	public ResultSet haeEsimKanta(){
+        return true;
 
-		ResultSet rs = null; // Kyselyn tulokset
+    }
 
 
-		try {
-			
-			String lause = "SELECT * " + "FROM esimkanta;";
+   //Yhteyden sulkeminen
+   public boolean suljeYhteys(){
 
-			rs = lahetaKysely(lause);
+        // yhteyden sulkeminen 
+    
+        if (con != null) { // jos yhteyden luominen ei onnistunut, con == null
 
-		} catch (Exception poikkeus) {
-			System.out.println("Jokin meni pieleen.");
-			return null;
-		}
+            try {     
+                con.close();
+                stmt.close(); // Sulkee myös tulosjoukon    
+                return true;
 
-		return rs; //Palautetaan tulosjoukko
+            } catch(SQLException poikkeus) {
+                System.out.println("Yhteyden sulkeminen tietokantaan ei onnistunut. Lopetetaan ohjelman suoritus.");
 
-	}
-   //Hakee tehtävälistan tehtävien lukumäärän
-   public int haeTLkm(int tehtavaSarja){
-      
-      ResultSet rs = lahetaKysely("KYSELY TÄNNE");
-      
-      /*
-         Käsitellään kyselyn tulos
-      */
-      
-      return 0;
+                return false; //null
+            }
+
+        }else{
+            System.out.println("Yhteyttä tietokantaan ei ollut. Yhteyden sulkeminen tietokantaan ei onnistunut.");
+            return false;
+        }
+    }
+
+
+    //Haetaan tehtavalista
+    public ResultSet haeTehtLista(int listaNro){
+
+        ResultSet rs = null; // Kyselyn tulokset
+
+        try {
+        
+            String lause = "SELECT id, kuvaus FROM tehtavalista WHERE id = " + listaNro + ";";
+            rs = lahetaKysely(lause);
+
+        } catch (Exception poikkeus) {
+            System.out.println(VIRHE);
+            return null;
+        }
+
+        return rs; //Palautetaan tulosjoukko
+
    }
 
-	//Laheta Kysely
-	public ResultSet lahetaKysely(String kysely){
+    //Haetaan tehtävä
+    public ResultSet haeTehtava(int tehtNro, int tehtLista){
+
+        ResultSet rs = null; // Kyselyn tulokset
+        try {            
+        String lause = "SELECT tehtava.id, tehtava.kuvaus " + 
+            "FROM tehtava, kuuluu, tehtavalista " + 
+            "WHERE tehtava.id = " + tehtNro + 
+            " AND tehtavalista.id = " + tehtLista + 
+            " AND tehtava.id = kuuluu.tehtava_id AND kuuluu.tehtavalista_id = tehtavalista.id;";
+
+        rs = lahetaKysely(lause);
+
+        } catch (Exception poikkeus) {
+            System.out.println(VIRHE);
+            return null;
+        }
+        
+        return rs; //Palautetaan tulosjoukko
+
+   }
+
+    //Hae esimerkkitietokanta
+    public ResultSet haeEsimKanta(){
+
+        ResultSet rs = null; // Kyselyn tulokset
+
+        
+        try {
+         
+        String lause = "SELECT * " + "FROM esimkanta;";
+
+        rs = lahetaKysely(lause);
+
+        } catch (Exception poikkeus) {
+            System.out.println(VIRHE);
+            return null;
+        }
+
+        return rs; //Palautetaan tulosjoukko
+
+   }
+    //Hakee tehtävälistan tehtävien lukumäärän
+    public int haeTLkm(int tLista){
+      
+        int palautus = 0;
+        ResultSet rs = lahetaKysely("SELECT teht_lkm FROM tehtavalista WHERE id = " + tLista + ";");
+        /*
+        try{
+            palautus = rs.getInt("teht_lkm");
+        }
+        catch (SQLException e) {
+            System.out.println(VIRHE);
+            return 0;
+        }
+      
+        return palautus;
+        */
+    }
+
+    //Laheta Kysely
+    public ResultSet lahetaKysely(String kysely){
+
+        ResultSet rs = null; // Kyselyn tulokset
+
+        if (tarkistaSyntaksi(kysely)) { //tsiigataan onko kyselyn syntaksi oikeellinen
+
+            try {
+
+                stmt = con.createStatement();
+
+                // Tarkistetaan onko kyselyssä syntaksivirheitä
+                if(tarkistaSyntaksi(kysely)){
+
+                    rs = stmt.executeQuery(kysely);
+                    return rs;
+
+                }else {
+                    rs = null;
+                    return rs;
+                }
+
+            // Toiminta mahdollisessa virhetilanteessa
+            } catch (SQLException poikkeus) {
+                System.out.println("Tapahtui seuraava virhe: " + poikkeus.getMessage());     
+                return null;
+            }
+
+        } else {
+            System.out.println("Kyselyssä on virheellinen syntaksi. Tarkista mahdolliset kirjoitusvirheet.");
+            return null;
+        }
+    }
+
+    //Tarkistaa kyselyn syntaksivirheiden varalta
+    public boolean tarkistaSyntaksi(String kysely){
+
+        char merkki;
+        int avausLaskuri = 0;
+        int sulkuLaskuri = 0;
+        boolean palautus;
+
+        palautus = true;
+
+        for(int i = 0; i < kysely.length(); i++){
+
+            merkki = kysely.charAt(i);
+
+            if (merkki == '(') {
+                avausLaskuri++;  
+            }
+            if (merkki == ')') {
+                sulkuLaskuri++;
+            }
+        }
 
-		ResultSet rs = null; // Kyselyn tulokset
+        if (avausLaskuri != sulkuLaskuri) {
+            System.out.println("Kyselyssä täytyy olla parillinen määrä kaarisulkeita.");
+            palautus = false;
+        }
+
+        merkki = kysely.charAt(kysely.length() - 1);
+
+        if(merkki != ';'){
+            System.out.println("Kyselyn täytyy päättyä puolipisteeseen.");
+            palautus = false;
+        }
+
+      return palautus;
+   }
+
+    //Vertaa kahta ResultSettiä.
+    public boolean vertaaTulokset(ResultSet rs, ResultSet esim){
+
+        String tulos;
+        String vastaus;
+        int i;
+        boolean palautus;
 
-		if (tarkistaSyntaksi(kysely)) { //tsiigataan onko kyselyn syntaksi oikeellinen
+        i = 0;
+        palautus = true;
+        try {
+            while(rs.next() | esim.next()){
+             
+                i++;
+             
+                tulos = rs.getString(i);
+                vastaus = esim.getString(i);
 
-			try {
+                if(!(tulos.equals(vastaus))){
+                    palautus = false;
+                }
+
+            }
+
+            //Vastaus oli väärä.
+            if (palautus == false) {
+             
+                System.out.println("Kyselyssä oli looginen virhe.\nVastauksesi palautti tuloksen:\n");
+             
+                i = 0;
+
+                while(rs.next()){
+                    i++;
+                    System.out.println(rs.getString(i));
+        
+                }
+
+                System.out.println("Kyselyn tulisi tuottaa tulos:\n");
+             
+                i = 0;
+
+                while(esim.next()){
+                    i++;
+                    System.out.println(esim.getString(i));
+                }
+                return palautus;
+            }
+        }
+        catch (SQLException poikkeus) {
+            System.out.println("Tapahtui seuraava virhe: " + poikkeus.getMessage());     
+        }       
 
-				stmt = con.createStatement();
+        return palautus;
 
-				// Tarkistetaan onko kyselyssä syntaksivirheitä
-				if(tarkistaSyntaksi(kysely)){
+   }
 
-					rs = stmt.executeQuery(kysely);
-					return rs;
+    /*Parametrinä tehtävälistan id*/
+    public boolean onkoTehtavalistaOlemassa(int id){
+
+        try {
 
-				}else {
-					rs = null;
-					return rs;
-				}
+            final String tehtavalistaTarkistus = "SELECT count(*) FROM tehtavalista WHERE id = " + id + ";";
+
+            ResultSet rs = lahetaKysely(tehtavalistaTarkistus);
 
-			// Toiminta mahdollisessa virhetilanteessa
-			} catch (SQLException poikkeus) {
-				System.out.println("Tapahtui seuraava virhe: " + poikkeus.getMessage());     
-				return null;
-			}
-
-		} else {
-			System.out.println("Kyselyssä on virheellinen syntaksi. Tarkista mahdolliset kirjoitusvirheet.");
-			return null;
-		}
-	}
-
-	//Tarkistaa kyselyn syntaksivirheiden varalta
-	public boolean tarkistaSyntaksi(String kysely){
-
-		char merkki;
-		int avausLaskuri = 0;
-		int sulkuLaskuri = 0;
-		boolean palautus;
-
-		palautus = true;
-
-		for(int i = 0; i < kysely.length(); i++){
-
-			merkki = kysely.charAt(i);
-
-			if (merkki == '(') {
-				avausLaskuri++;  
-			}
-			if (merkki == ')') {
-				sulkuLaskuri++;
-			}
-		}
-
-		if (avausLaskuri != sulkuLaskuri) {
-			System.out.println("Kyselyssä täytyy olla parillinen määrä kaarisulkeita.");
-			palautus = false;
-		}
-
-		merkki = kysely.charAt(kysely.length() - 1);
-
-		if(merkki != ';'){
-			System.out.println("Kyselyn täytyy päättyä puolipisteeseen.");
-			palautus = false;
-		}
-
-		return palautus;
-	}
-
-	//Vertaa kahta ResultSettiä.
-	public boolean vertaaTulokset(ResultSet rs, ResultSet esim){
-
-		String tulos;
-		String vastaus;
-		int i;
-		boolean palautus;
-
-		i = 0;
-		palautus = true;
-		try {
-		while(rs.next() | esim.next()){
-			
-			i++;
-			
-			tulos = rs.getString(i);
-			vastaus = esim.getString(i);
-
-			if(!(tulos.equals(vastaus))){
-				palautus = false;
-			}
-
-		}
-
-		//Vastaus oli väärä.
-		if (palautus == false) {
-			
-			System.out.println("Kyselyssä oli looginen virhe.\nVastauksesi palautti tuloksen:\n");
-			
-			i = 0;
-
-			while(rs.next()){
-				i++;
-				System.out.println(rs.getString(i));
-
-			}
-
-			System.out.println("Kyselyn tulisi tuottaa tulos:\n");
-			
-			i = 0;
-
-			while(esim.next()){
-				i++;
-				System.out.println(esim.getString(i));
-
-			}
-
-			return palautus;
-		}
-		}
-		catch (SQLException poikkeus) {
-			System.out.println("Tapahtui seuraava virhe: " + poikkeus.getMessage());     
-		}       
-
-		return palautus;
-
-
-
-	}
-
-	/*Parametrinä tehtävälistan id*/
-	public boolean onkoTehtavalistaOlemassa(int id){
-
-		try {
-
-			final String tehtavalistaTarkistus = "SELECT count(*) FROM tehtavalista WHERE id = " + id + ";";
-
-			ResultSet rs = lahetaKysely(tehtavalistaTarkistus);
-
-			if (rs.next()) {
-				if(rs.getInt(1) > 0){
-					return true;
-				}
-			}
-
-			return false;
-
-		}catch(SQLException poikkeus){
-			System.out.println("Tapahtui bugi.");
-			return false;
-		}
-	}
-
-	public boolean onkoTehtavaOlemassa(int id){
-		try{
-
-			final String tehtavaTarkistus = "SELECT count(*) FROM tehtava WHERE id = " + id + ";";
-
-			ResultSet rs = lahetaKysely(tehtavaTarkistus);
-
-			if(rs.next()){
-				if(rs.getInt(1) > 0){
-					return true;
-				}
-			}
-
-			return false;
-
-		}catch(SQLException poikkeus){
-			System.out.println("Tapahtui bugi.");
-			return false;
-		}
-	}
-
-	/*Parametrinä käyttäjätunnus, eli id*/
-	public boolean onkoKayttajaOlemassa(int id){
-
-		try{
-
-			final String tunnusTarkistus = "SELECT count(*) FROM kayttaja WHERE id = " + id + ";";
-
-			ResultSet rs = lahetaKysely(tunnusTarkistus);
-
-			if(rs.next()){
-				if(rs.getInt(1) > 0){
-					return true;
-				}
-			}
-			return false;
-
-		}catch(SQLException poikkeus){
-			System.out.println("Tapahtui bugi.");
-			return false;
-		}
-	}
-
-	/*palauttaa 0 jos ei löydy*/
-	public int haeOikeudet(int id){
-
-		try{
-
-			String oikeudetTarkistus = "SELECT oikeudet FROM kayttaja WHERE id = " + id + ";";
-
-			ResultSet rs = lahetaKysely(oikeudetTarkistus);
-
-			if(rs.next()){
-				return rs.getInt(1);
-			}
-
-			return 0;
-
-		}catch(SQLException poikkeus){
-			System.out.println("Tapahtui bugi.");
-			return 0;
-		}
-	}
-
-	/*Parametrinä käyttäjätunnus: k_id ja tehtävälistatunnus: t_id*/
-	public int aloitaSessio(int k_id, int t_id){
-
-		try{
-
-			//hakee isoimman sessionin id:n resultsettiin
-			ResultSet rs = lahetaKysely("SELECT max(id) FROM sessio;");
-
-			//tähän tallennetaan uusi ID
-			int uusiID = 0;
-
-			if(rs.next()){
-				//kasvatetaan ID:tä yhellä
-				uusiID = rs.getInt(1)+1;
-
-			}
-			//ja tähän tämän hetken aika
-			java.sql.Time aika = haeAika();
-
-			//puuttuu tuo yristen numero
-			//puuttuu osaa
-			String sessioAloitus = "INSERT INTO sessio VALUES (" + uusiID + ", " + k_id + ", " + t_id + ", " + aika + ", " + aika + ");";
-
-			if(rs.next()){
-
-				return rs.getInt(1);
-			}
-			return 0;
-
-		}catch(SQLException poikkeus){
-			System.out.println("Tapahtui bugi.");
-			return 0;
-		}
-	}
-
-	/*hakee ja palauttaa tämänhetkisen ajan, eli pelkän ajan, ei pvm*/
+            if (rs.next()) {
+                if(rs.getInt(1) > 0){
+                    return true;
+                }
+            }
+
+        return false;
+
+        }catch(SQLException poikkeus){
+            System.out.println(VIRHE);
+            return false;
+        }
+    }
+
+    public boolean onkoTehtavaOlemassa(int id){
+        try{
+
+            final String tehtavaTarkistus = "SELECT count(*) FROM tehtava WHERE id = " + id + ";";
+
+            ResultSet rs = lahetaKysely(tehtavaTarkistus);
+    
+            if(rs.next()){
+                if(rs.getInt(1) > 0){
+                return true;
+                }
+            }
+
+            return false;
+
+        }catch(SQLException poikkeus){
+            System.out.println(VIRHE);
+            return false;
+        }
+    }
+
+    /*Parametrinä käyttäjätunnus, eli id*/
+    public boolean onkoKayttajaOlemassa(int id){
+
+        try{
+
+            final String tunnusTarkistus = "SELECT count(*) FROM kayttaja WHERE id = " + id + ";";
+
+            ResultSet rs = lahetaKysely(tunnusTarkistus);
+
+            if(rs.next()){
+                if(rs.getInt(1) > 0){
+                return true;
+            }
+        }
+        return false;
+
+        }catch(SQLException poikkeus){
+            System.out.println(VIRHE);
+            return false;
+        }
+    }
+
+    /*palauttaa 0 jos ei löydy*/
+    public int haeOikeudet(int id){
+
+        try{
+            String oikeudetTarkistus = "SELECT oikeudet FROM kayttaja WHERE id = " + id + ";";
+            ResultSet rs = lahetaKysely(oikeudetTarkistus);
+
+            if(rs.next()){
+                return rs.getInt(1);
+            }
+            return 0;
+
+        }catch(SQLException poikkeus){
+            System.out.println(VIRHE);
+            return 0;
+        }
+    }
+
+    /*Parametrinä käyttäjätunnus: k_id ja tehtävälistatunnus: t_id*/
+    public int aloitaSessio(int k_id, int t_id){
+
+        try{
+
+            //hakee isoimman sessionin id:n resultsettiin
+            ResultSet rs = lahetaKysely("SELECT max(id) FROM sessio;");
+
+            //tähän tallennetaan uusi ID
+            int uusiID = 0;
+
+            if(rs.next()){
+                //kasvatetaan ID:tä yhellä
+                uusiID = rs.getInt(1)+1;
+
+            }
+            //ja tähän tämän hetken aika
+            java.sql.Time aika = haeAika();
+
+            //puuttuu tuo yristen numero
+            //puuttuu osaa
+            String sessioAloitus = "INSERT INTO sessio VALUES (" + uusiID + ", " + k_id + ", " + t_id + ", " + aika + ", " + aika + ");";
+
+            if(rs.next()){
+                return rs.getInt(1);
+            }
+            return 0;
+
+        }catch(SQLException poikkeus){
+            System.out.println(VIRHE);
+            return 0;
+        }
+    }
+
+    /*hakee ja palauttaa tämänhetkisen ajan, eli pelkän ajan, ei pvm*/
     public java.sql.Time haeAika(){
         java.util.Date a = new java.util.Date();
         long aikaL = a.getTime();
