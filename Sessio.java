fd573fd2f5c440b4e6c487ab80b418439299f8ba
bamsse <jenni.mansikka-aho@uta.fi>
2014-05-11 21:15:54 +0300

    sisennykset korjattu

--- Sessio.java
+++ Sessio.java
@@ -1,137 +1,129 @@
-
-
-
 import java.sql.*;
-import java.util.Scanner;
-
 
 public class Sessio {
    
-   private TietokantaToiminnot db;
+	private TietokantaToiminnot db;
    
-   public void suoritaSessio(){
-      //Onko käyttäjä kirjautunut sisään
-      boolean kirjautunut = false; 
+	public void suoritaSessio(){
+		//Onko käyttäjä kirjautunut sisään
+		boolean kirjautunut = false; 
       
-      //Avataan db yhteys, jos epäonnistuu lopetetaan
-      db = new TietokantaToiminnot();
-      if(!db.avaaYhteys()){
-         return;
-      }
+		//Avataan db yhteys, jos epäonnistuu lopetetaan
+		db = new TietokantaToiminnot();
+		if(!db.avaaYhteys()){
+			return;
+		}
       
-      //Kysytään käyttäjältä tunnuksia kunnes kirjautuminen onnistuu
-      while(!kirjautunut){
-         kirjautunut = kirjaus();
-      }
+		//Kysytään käyttäjältä tunnuksia kunnes kirjautuminen onnistuu
+		while(!kirjautunut){
+			kirjautunut = kirjaus();
+		}
       
-      //Kirjautuminen onnistui
-      if(kirjautunut){
+		//Kirjautuminen onnistui
+		if(kirjautunut){
 
-         while(true){
+			while(true){
+		  
+				/*
+				 * tulostaListat();
+				 */
+			 
+				//Pyydetään valinta käyttäjältä ja toimitaan sen mukaan
+				System.out.println("Mikäli haluat nähdä tehtävälistan, kirjoita 1. Mikäli haluat suorittaa tehtävälistan, kirjoita 2. Lopettaaksesi kirjoita 0");
+				int valinta = In.readInt();
+				
+				//Haetaan tehtavalista
+				if(valinta == 1){
+					boolean numeroOK = false;
+					int tehtavaListaNumero;
+					while(!numeroOK){
+					  
+						System.out.println("Anna tehtävälistan numero:");
+						tehtavaListaNumero = In.readInt();
+					  
+						if(db.onkoTehtavalistaOlemassa(tehtavaListaNumero)){
+
+							numeroOK = true;
+							ResultSet tehtavalista = db.haeTehtLista(tehtavaListaNumero);
+							/*tähän tulostukset*/
+							tulostaTLista(tehtavalista);
+					  
+						}
+					}
+				}
+				//Suoritetaan tehtavalista
+				else if(valinta == 2){
+				
+					boolean numeroOK = false;
+					while(!numeroOK){
+
+						System.out.println("Anna tehtävälistan numero:");
+						int tehtavaListaNumero = In.readInt();
+						//Jos tehtavalista löytyy, suoritetaan se
+						if(db.onkoTehtavaOlemassa(tehtavaListaNumero)){
+							numeroOK = true;
+							suoritaTLista(tehtavaListaNumero);
+						}
+					}
+				}
+			}
+		}
+		db.suljeYhteys();
+	}
+	
+	//Tulostaa tehtavalistan
+	public void tulostaTLista(ResultSet rs){
+
+	}
+   
+	//Tulostaa tarjolla olevat tehtavalistat
+	public void tulostaListat(){
+
+	}
+
+
+	//Tehtavalistan suorittaminen
+	public void suoritaTLista(int tlNro){
       
-            /*
-            tulostaListat();
-            */
+		ResultSet tehtavalista = null;
+		int tlPituus = db.haeTLkm(tlNro);
+		//Käydään listan tehtävät läpi
+		for (int i = 0; i < tlPituus; i++){
          
-            //Pyydetään valinta käyttäjältä ja toimitaan sen mukaan
-            System.out.println("Mikäli haluat nähdä tehtävälistan, kirjoita 1. Mikäli haluat suorittaa tehtävälistan, kirjoita 2. Lopettaaksesi kirjoita 0");
-            int valinta = In.readInt();
-            
-            //Haetaan tehtavalista
-            if(valinta == 1){
-               boolean numeroOK = false;
-               int tehtavaListaNumero;
-               while(!numeroOK){
-                  
-                  System.out.println("Anna tehtävälistan numero:");
-                  tehtavaListaNumero = In.readInt();
-                  
-                  if(db.onkoTehtavalistaOlemassa(tehtavaListaNumero)){
+			tehtavalista = db.haeTehtava(i, tlNro);
+			/*tähän tulostukset ja jatkokyselyt*/
+ 
+			int j = 0;
+			// Tulostetaan tehtävän kuvaus:
+			try {
+				while (tehtavalista.next()) {
+					j++;
+					System.out.println (tehtavalista.getString(j));
+				}
+			}
+			catch (SQLException e) {
+				System.out.println("Tapahtui virhe.");
+			}
 
-                     numeroOK = true;
-                     ResultSet tehtavalista = db.haeTehtLista(tehtavaListaNumero);
-                     /*tähän tulostukset*/
-                     tulostaTLista(tehtavalista);
-                  
-                  }
-               }
-            }
-            //Suoritetaan tehtavalista
-            else if(valinta == 2){
-            
-               boolean numeroOK = false;
-               while(!numeroOK){
+		}
+	}
 
-                  System.out.println("Anna tehtävälistan numero:");
-                  int tehtavaListaNumero = In.readInt();
-                     //Jos tehtavalista löytyy, suoritetaan se
-                  if(db.onkoTehtavaOlemassa(tehtavaListaNumero)){
+	//Kirjaa käyttäjän sisään
+	public boolean kirjaus(){
 
-                     numeroOK = true;
-                     suoritaTLista(tehtavaListaNumero);
-
-                  }
-               }
-            }
-         }
-      }
-      db.suljeYhteys();
-   }
-   
-   //Tulostaa tehtavalistan
-   public void tulostaTLista(ResultSet rs){
-   
-   
-   }
-   
-   //Tulostaa tarjolla olevat tehtavalistat
-   public void tulostaListat(){
-   
-   
-   }
-   
-   
-   //Tehtavalistan suorittaminen
-   public void suoritaTLista(int tlNro){
+		int kayttajatunnus = 0;
       
-      ResultSet tehtavalista = null;
-      int tlPituus = db.haeTLkm(tlNro);
-      //Käydään listan tehtävät läpi
-      for (int i = 0; i < tlPituus; i++){
-         
-         tehtavalista = db.haeTehtava(i, tlNro);
-         /*tähän tulostukset ja jatkokyselyt*/
-         
-         int j = 0;
-         // Tulostetaan tehtävän kuvaus:
-         try {
-            while (tehtavalista.next()) {
-               j++;
-               System.out.println (tehtavalista.getString(j));
-            }
-         }
-         catch (SQLException e) {
-             System.out.println("Tapahtui virhe.");
-         }
-      
-      }
-   }
-   
-   //Kirjaa käyttäjän sisään
-   public boolean kirjaus(){
-      
-      int kayttajatunnus = 0;
-      
-      System.out.println("Käyttäjätunnus:");
-      kayttajatunnus = In.readInt();
+		System.out.println("Käyttäjätunnus:");
+		kayttajatunnus = In.readInt();
 
-      if(!db.onkoKayttajaOlemassa(kayttajatunnus)){ //jos arvoa ei löytynyt
-         System.out.println("Käyttäjätunnusta ei löytynyt. Yritä uudelleen.");
-         return false;
+		if(!db.onkoKayttajaOlemassa(kayttajatunnus)){ //jos arvoa ei löytynyt
+			System.out.println("Käyttäjätunnusta ei löytynyt. Yritä uudelleen.");
+			return false;
 
-      }else{
-         System.out.println("Sisäänkirjautuminen onnistui!");
-         return true;
-      }
-   }
+		}
+		else {
+			System.out.println("Sisäänkirjautuminen onnistui!");
+			return true;
+		}
+	}
 }
